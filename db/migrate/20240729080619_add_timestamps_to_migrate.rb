c9c8cfb37460b6548fded8c2b31ecb97a70189ec
BitSlayer18 <nikhil.agar147@gmail.com>
2024-07-29 15:35:15 +0530

    Added validations and model(article) class

--- db/migrate/20240729080619_add_timestamps_to_migrate.rb
+++ db/migrate/20240729080619_add_timestamps_to_migrate.rb
@@ -2,6 +2,5 @@ class AddTimestampsToMigrate < ActiveRecord::Migration[7.1]
   def change
     add_column :articles, :created_at,:datetime
     add_column :articles, :updated_at,:datetime
-
   end
 end
