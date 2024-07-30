d3b588c8298524d7a049746ae7f3fed7c382f548
BitSlayer18 <nikhil.agar147@gmail.com>
2024-07-30 12:03:27 +0530

    Added method for creating new files

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title,presence: true,length: {minimum: 6,maximum: 20}
-  validates :description , presence: true,length: {minimum: 50,maximum: 200}
+  validates :title,presence: true,length: {minimum: 2,maximum: 20}
+  validates :description , presence: true,length: {minimum: 5,maximum: 200}
 end
