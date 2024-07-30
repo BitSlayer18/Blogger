fbaeb31c2f846ed2bc41a85e45318272f3beb254
BitSlayer18 <nikhil.agar147@gmail.com>
2024-07-30 17:26:18 +0530

    Added the partial to and DRY methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :get_id,only: [:edit,:update,:show,:destroy]
   def show
-    @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -9,10 +9,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-    @article = Article.find(params[:id])
   end
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -21,17 +20,26 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
       render 'edit'
     end
   end
-  def destory
-    @article = Article.find(params[:id])
-    @article.destory
+  #the earlier version was not working because i had set destroy as destory othewise they are fine and will be working
+  def destroy
+    @article.destroy
     redirect_to articles_path
+  end
+
+  private #don't add methods that you want to use outside the controller as that won't work
+
+  def get_id
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
