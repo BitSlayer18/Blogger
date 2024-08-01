a34ea4a97a274ea1717f99cf6288e0917afd36b9
BitSlayer18 <nikhil.agar147@gmail.com>
2024-08-01 16:14:45 +0530

    Added the show page and flash messages layouts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#So that the class field or error is skipped and not create an issue
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
