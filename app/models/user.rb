a2d6c23d3e6e62429c524e39902aa0181102750b
BitSlayer18 <nikhil.agar147@gmail.com>
2024-08-02 12:44:15 +0530

    Added password authentication to backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
   has_many :articles
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username,presence: true,length: {minimum: 3,maximum: 25},uniqueness: {case_sensitive: false}
   validates :email,presence: true,length: {maximum: 105},uniqueness: {case_sensitive: false},format: {with: VALID_EMAIL}
+  has_secure_password
 end
