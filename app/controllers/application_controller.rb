eef3355907cf4e5a347f2800eef2a394ed880d09
BitSlayer18 <nikhil.agar147@gmail.com>
2024-08-04 17:42:35 +0530

    Some helper method and layout cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user,:logged_in?
+  def current_user
+    #the || helps in retreiving value from database if current user has been retrieved earlier
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    #turning the current user into a boolean
+    !!current_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user,only: [:show,:edit,:update]
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "new User Created Succesfully"
       redirect_to articles_path
     else
@@ -19,12 +20,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 2)
   end
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.save
       flash[:notice] = "User details were updated succesfully"
       redirect_to @user
@@ -37,5 +36,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
