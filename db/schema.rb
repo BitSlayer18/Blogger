e16a9517271cb7dc00ce9d4fee6b0181787e5ebf
BitSlayer18 <nikhil.agar147@gmail.com>
2024-08-05 16:25:43 +0530

    Added many_to_many association of categories and articles from backend and frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_055429) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_101118) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
